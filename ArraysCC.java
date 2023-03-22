ec87ba690d58893ef09873e55e25d6b4d78704e5
Prashant <ppy.ppy.1997@gmail.com>
2023-03-22 11:55:57 +0530

    buy sell stock question

--- ArraysCC.java
+++ ArraysCC.java
@@ -29,9 +29,32 @@ public class ArraysCC {
         
         return trappedWater;
     }
+
+    //buy sell stocks
+
+    public static int buySellStocks(int prices[]){
+        int buyPrice=Integer.MAX_VALUE;
+        int maxProfit =0;
+        for(int i=0; i<prices.length;i++){
+            if(buyPrice < prices[i]){//profit
+                int profit = prices[i]-buyPrice;//todays profit
+                maxProfit = Math.max(maxProfit, profit); //global profit
+            }else{
+                buyPrice=prices[i];
+            }
+        }
+
+    return  maxProfit;
+    }
     
     public static void main(String args[]){
-        int height[]={4,2,0,6,3,2,5};
-        System.out.println(trappedRainWater(height));
+        
+                    //buy and sell stocks
+        int prices[]={7,1,5,3,6,4};
+        System.out.println(buySellStocks(prices));
+        
+                    //traped water
+        // int height[]={4,2,0,6,3,2,5};
+        // System.out.println(trappedRainWater(height));
     }
 }
